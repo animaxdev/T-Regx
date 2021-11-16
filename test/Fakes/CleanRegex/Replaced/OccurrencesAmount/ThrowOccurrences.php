430b69d3490ba28571989ab8121eded6ba4d6a55
Daniel Wilkowski <wilkowski.kontakt@gmail.com>
2021-11-17 00:07:49 +0100

    Revamp of Replace module, TDD style, exactly()

---
+++ test/Fakes/CleanRegex/Replaced/OccurrencesAmount/ThrowOccurrences.php
@@ -0,0 +1,22 @@
+<?php
+namespace Test\Fakes\CleanRegex\Replaced\OccurrencesAmount;
+
+use Test\Fakes\CleanRegex\Internal\ThrowSubject;
+use Test\Utils\Fails;
+use TRegx\CleanRegex\Internal\Definition;
+use TRegx\CleanRegex\Replaced\OccurrencesAmount;
+
+class ThrowOccurrences extends OccurrencesAmount
+{
+    use Fails;
+
+    public function __construct()
+    {
+        parent::__construct(new Definition('', ''), new ThrowSubject(), -1);
+    }
+
+    public function count(): int
+    {
+        throw $this->fail();
+    }
+}
---
+++ test/Unit/CleanRegex/Replaced/ExactListenerTest.php
@@ -0,0 +1,24 @@
+<?php
+namespace Test\Unit\TRegx\CleanRegex\Replaced;
+
+use PHPUnit\Framework\TestCase;
+use TRegx\CleanRegex\Exception\ReplacementExpectationFailedException;
+use TRegx\CleanRegex\Replaced\ExactListener;
+
+class ExactListenerTest extends TestCase
+{
+    /**
+     * @test
+     */
+    public function shouldNotUnnecessarilyCountOccurrences()
+    {
+        // given
+        $listener = new ExactListener(new \Test\Fakes\CleanRegex\Replaced\OccurrencesAmount\ThrowOccurrences(), 4);
+
+        // then
+        $this->expectException(ReplacementExpectationFailedException::class);
+
+        // when
+        $listener->replaced(3);
+    }
+}
