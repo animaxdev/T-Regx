749802851ba799ca74e19b90e964387c9cb70d33
Daniel Wilkowski <wilkowski.kontakt@gmail.com>
2021-11-20 22:57:13 +0100

    Extracted ReplacementFunction

--- src/CleanRegex/Replaced/ReplacerCallback.php
+++ src/CleanRegex/Replaced/ReplacerCallback.php
@@ -1,40 +1,20 @@
 <?php
 namespace TRegx\CleanRegex\Replaced;
 
-use TRegx\CleanRegex\Exception\InvalidReplacementException;
-use TRegx\CleanRegex\Internal\Definition;
-use TRegx\CleanRegex\Internal\Subject;
-use TRegx\CleanRegex\Internal\Type\ValueType;
-use TRegx\SafeRegex\preg;
-
 class ReplacerCallback
 {
-    /** @var Definition */
-    private $definition;
-    /** @var Subject */
-    private $subject;
-    /** @var int */
-    private $limit;
+    /** @var CalledBack */
+    private $calledBack;
 
-    public function __construct(Definition $definition, Subject $subject, int $limit)
+    public function __construct(CalledBack $calledBack)
     {
-        $this->definition = $definition;
-        $this->subject = $subject;
-        $this->limit = $limit;
+        $this->calledBack = $calledBack;
     }
 
-    public function replace(callable $replacer): string
+    public function replaced(ReplacementFunction $function): string
     {
-        return preg::replace_callback($this->definition->pattern, function (array $matches) use ($replacer): string {
-            return $this->replacement($replacer($matches[0]));
-        }, $this->subject->getSubject(), $this->limit);
-    }
-
-    public function replacement($replacement): string
-    {
-        if (\is_string($replacement)) {
-            return $replacement;
-        }
-        throw new InvalidReplacementException(new ValueType($replacement));
+        return $this->calledBack->replaced(static function (array $matches) use ($function): string {
+            return $function->apply($matches[0]);
+        });
     }
 }
