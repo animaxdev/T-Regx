ffdc3db02130b627e77273a946b6953e838d7616
Daniel Wilkowski <wilkowski.kontakt@gmail.com>
2021-11-20 20:20:53 +0100

    Extract Replacements

--- src/CleanRegex/Replaced/ReplacerByMap.php
+++ src/CleanRegex/Replaced/ReplacerByMap.php
@@ -2,17 +2,12 @@
 namespace TRegx\CleanRegex\Replaced;
 
 use TRegx\CleanRegex\Exception\GroupNotMatchedException;
-use TRegx\CleanRegex\Exception\MissingReplacementKeyException;
 use TRegx\CleanRegex\Exception\NonexistentGroupException;
 use TRegx\CleanRegex\Internal\Definition;
 use TRegx\CleanRegex\Internal\GroupKey\GroupKey;
-use TRegx\CleanRegex\Internal\InvalidArgument;
-use TRegx\CleanRegex\Internal\Message\Replace\Map\ForGroupMessage;
-use TRegx\CleanRegex\Internal\Message\Replace\Map\ForMatchMessage;
 use TRegx\CleanRegex\Internal\Model\GroupAware;
 use TRegx\CleanRegex\Internal\Model\LightweightGroupAware;
 use TRegx\CleanRegex\Internal\Subject;
-use TRegx\CleanRegex\Internal\Type\ValueType;
 use TRegx\SafeRegex\preg;
 
 class ReplacerByMap
@@ -36,10 +31,18 @@ class ReplacerByMap
 
     public function byMap(GroupKey $group, array $replacements, MissingGroupHandler $handler): string
     {
-        $this->validate($replacements);
-        $result = preg::replace_callback($this->definition->pattern, function (array $matches) use ($handler, $group, $replacements): string {
-            return $this->replaceGroup($group, $matches, $replacements, $handler);
-        }, $this->subject->getSubject(), $this->limit, $count);
+        return $this->replaceByMap($handler, $group, new Replacements($replacements));
+    }
+
+    private function replaceByMap(MissingGroupHandler $handler, GroupKey $group, Replacements $replacements)
+    {
+        $result = preg::replace_callback($this->definition->pattern,
+            function (array $matches) use ($handler, $group, $replacements): string {
+                return $this->replaceGroup($group, $matches, $replacements, $handler);
+            },
+            $this->subject->getSubject(),
+            $this->limit,
+            $count);
         if ($count === 0) {
             if (!$this->groupAware->hasGroup($group->nameOrIndex())) {
                 throw new NonexistentGroupException($group);
@@ -48,7 +51,7 @@ class ReplacerByMap
         return $result;
     }
 
-    private function replaceGroup(GroupKey $group, array $matches, array $replacements, MissingGroupHandler $handler): string
+    private function replaceGroup(GroupKey $group, array $matches, Replacements $replacements, MissingGroupHandler $handler): string
     {
         if (!\array_key_exists($group->nameOrIndex(), $matches)) {
             if ($this->groupAware->hasGroup($group->nameOrIndex())) {
@@ -59,32 +62,6 @@ class ReplacerByMap
         if ($matches[$group->nameOrIndex()] === '') {
             throw GroupNotMatchedException::forReplacement($group);
         }
-        return $this->replacement($matches, $group, $replacements);
-    }
-
-    private function replacement(array $matches, GroupKey $group, array $replacements): string
-    {
-        $occurrence = $matches[$group->nameOrIndex()];
-        if (\array_key_exists($occurrence, $replacements)) {
-            return $replacements[$occurrence];
-        }
-        throw new MissingReplacementKeyException($this->message($group, $matches[0], $occurrence)->getMessage());
-    }
-
-    private function message(GroupKey $group, string $match, string $occurrence)
-    {
-        if ($group->full()) {
-            return new ForMatchMessage($match);
-        }
-        return new ForGroupMessage($match, $group, $occurrence);
-    }
-
-    private function validate(array $occurrencesAndReplacements): void
-    {
-        foreach ($occurrencesAndReplacements as $replacement) {
-            if (!\is_string($replacement)) {
-                throw InvalidArgument::typeGiven('Invalid replacement map value. Expected string', new ValueType($replacement));
-            }
-        }
+        return $replacements->replaced($group, $matches[$group->nameOrIndex()], $matches[0]);
     }
 }
