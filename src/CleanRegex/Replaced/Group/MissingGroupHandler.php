3081d6108dcaad7c648e3ce950d9ed7fa63f7a65
Daniel Wilkowski <wilkowski.kontakt@gmail.com>
2021-12-26 00:36:12 +0100

    WIP

--- src/CleanRegex/Replaced/Group/MissingGroupHandler.php
+++ src/CleanRegex/Replaced/Group/MissingGroupHandler.php
@@ -3,6 +3,10 @@ namespace TRegx\CleanRegex\Replaced\Group;
 
 use TRegx\CleanRegex\Internal\GroupKey\GroupKey;
 
+/**
+ * Please, rename it to something more OOP
+ * @deprecated
+ */
 interface MissingGroupHandler
 {
     public function handle(GroupKey $group, string $original): string;
